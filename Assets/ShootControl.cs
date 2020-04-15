98936d55fa3fb8cd2ad62cda14715c5b99525bd0
Luis Oliveira <dev.oliveiras@gmail.com>
2020-04-15 14:34:43 -0300

    Adicionado vento, edição de velocidade do tiro e angulo pelo editor.

--- Assets/ShootControl.cs
+++ Assets/ShootControl.cs
@@ -4,24 +4,33 @@ using UnityEngine;
 
 public class ShootControl : MonoBehaviour
 {
+    [Range(1,60)]
     public float velocity = 1.0f;
     public float angle = 45f;
 
-    public GameObject ball;
-    private Rigidbody2D ball_rb;
+    public GameObject ballObject;
     private Vector3 dir;
 
     // Start is called before the first frame update
     void Start()
     {
-        ball_rb = ball.GetComponent<Rigidbody2D>();
-        ball.transform.position = new Vector3(0.0f, -2.0f, 0.0f);
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetButtonDown("Jump"))
+        {
+            Shoot();
+        }
+    }
+
+    public void Shoot()
+    {
         dir = Quaternion.AngleAxis(angle, Vector3.forward) * Vector3.right;
-        ball_rb.AddForce(dir * velocity);
+        GameObject ball = Instantiate(ballObject,transform.position, Quaternion.identity);
+        Rigidbody2D ballrigidbody = ball.GetComponent<Rigidbody2D>();
+        ballrigidbody.AddForce(dir * velocity, ForceMode2D.Impulse);
     }
 }
---
+++ Assets/WindComponent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindComponent : MonoBehaviour
+{
+    public float force;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Physics2D.gravity = new Vector2(force, Physics2D.gravity.y);
+        }
+    }
+
+}
